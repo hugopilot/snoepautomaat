6b4e10c121b3ccf709dc74bdf3b38ff2065a3aca
Hugo Woesthuis <hugow2011@outlook.com>
2018-05-28 14:20:06 +0200

    add new values

--- integers.c
+++ integers.c
@@ -1,3 +1,8 @@
 // General
 const int GeneralServoOpenValue = 0;
 const int GeneralServoClosedValue = 90;
+
+// Buttons
+const int Button1_PIN = 7;
+const int Button2_PIN = 12;
+const int Button3_PIN = 13;
