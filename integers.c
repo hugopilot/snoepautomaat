e703e1ef86371a8f7818891f3320cbd807c5df40
Hugo Woesthuis <hugow2011@outlook.com>
2018-06-04 16:53:52 +0200

    Update integers.c

--- integers.c
+++ integers.c
@@ -6,3 +6,11 @@ const int GeneralServoClosedValue = 90;
 const int Button1_PIN = 7;
 const int Button2_PIN = 12;
 const int Button3_PIN = 13;
+
+// Servo Ports
+const int SBaan1_1 = 3;
+const int SBaan1_2 = 5;
+const int SBaan2_1 = 6;
+const int SBaan2_2 = 10;
+const int SBaan3_1 = 11;
+const int SBaan3_2 = 12;
