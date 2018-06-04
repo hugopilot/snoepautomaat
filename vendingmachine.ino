aeb256e4ac0633fe425c6a1d974fba0478e7a7e8
Hugo Woesthuis <hugow2011@outlook.com>
2018-06-04 16:49:33 +0200

    Update vendingmachine.ino

--- vendingmachine.ino
+++ vendingmachine.ino
@@ -6,21 +6,31 @@
 #include "Movements.cpp"
 #include "ServoHandler.cpp"
 #include "ServoPositions.c"
+#include "integers.c"
 #include "Vector.h"
 
 Vector<Product> pp;
-int a;
-int b;
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
-  // Meer setup
-  pp.push_back({GenerateServoClass(3), GenerateServoClass(5), 1.00, 3});
-  pp.push_back({GenerateServoClass(11), GenerateServoClass(12), 1.20, 512});
+  Serial.begin(57600);
+  
+  // Init producten, voor nu hardcoded
+  pp.push_back({GenerateServoClass(SBaan1_1), GenerateServoClass(SBaan1_2), 1.00, 3});
+  pp.push_back({GenerateServoClass(SBaan2_1), GenerateServoClass(SBaan2_2), 1.20, 512});
+  pp.push_back({GenerateServoClass(SBaan3_1), GenerateServoClass(SBaan3_2), 1.10, 512});
+
+  // Sluit alle poorten
   for(int i = 0; i < pp.size(); i++){
     Movements::ClosePorts(pp[i].PortServo, OnePortCL);
     Movements::ClosePorts(pp[i].HoldServo, OnePortCL);
   }
+
+  // Button pinModes set
+  pinMode(Button1_PIN, INPUT_PULLUP);
+  pinMode(Button2_PIN, INPUT_PULLUP);
+  pinMode(Button3_PIN, INPUT_PULLUP);
+
+  // Print succes bericht
   Serial.println("Setup Completed!");
   Serial.println("Vending machine started with success");  
   Serial.println(); 
@@ -29,38 +39,33 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
-  // Get analoge waarden
-  char c = Serial.read();
-  if(Serial.available() > 0){
-    if(c = 's'){
-    if(pp[0].CurrentQuantity <= 0){
-      Serial.println("Product is niet beschrikbaar");
-      }
-    else{
-      pp[0].CurrentQuantity --;
-      Movements::ReleaseProduct(pp[0], OnePortCL, OnePortOV);
-      Movements::RenewProduct(pp[0], OnePortCL, OnePortOV);
-      Serial.print("OK 1: ");
-      Serial.print(pp[0].CurrentQuantity);
-      Serial.println(" left.");
-      }
+  // Verkrijg digitale waarden
+  int b1State = digitalRead(Button1_PIN);
+  int b2State = digitalRead(Button2_PIN);
+  int b3State = digitalRead(Button3_PIN);
+  
+  if(b1State == HIGH){
+    if(pp[0].CurrentQuantity <= 1){
+      Movements::Release(pp[0], 90, 10);
     }
-    else if(c = 'a'){
-      if(pp[1].CurrentQuantity <= 0){
-        Serial.println("Product is niet beschrikbaar");
-      }
-      else{
-        pp[1].CurrentQuantity --;
-        Movements::ReleaseProduct(pp[1], OnePortCL, OnePortOV);
-        Movements::RenewProduct(pp[1], OnePortCL, OnePortOV);
-        Serial.print("OK 2: ");
-        Serial.print(pp[1].CurrentQuantity);
-        Serial.println(" left.");
-      }
+    else{
+      Movements::ReleaseAndRenew(pp[0], 90, 10);
+    }
+      
+  }
+  else if(b2State == HIGH){
+    if(pp[1].CurrentQuantity <= 1){
+      Movements::Release(pp[1], 90, 10);
+    }
+    else{
+      Movements::ReleaseAndRenew(pp[1], 90, 10);
+    }}
+    else if(b3State == HIGH){
+    if(pp[2].CurrentQuantity <= 1){
+      Movements::Release(pp[2], 90, 10);
+    }
+    else{
+      Movements::ReleaseAndRenew(pp[2], 90, 10);
     }
   }
-  // Stel een delay in om plotselinge release te voorkomen
-  //delay(5);
-  
-  
   }
